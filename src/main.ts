8aa326efbee2ea214a153ce5a0eeefd8e2157d05
Hoang Long <1612352@student.hcmus.edu.vn>
2020-05-26 18:53:02 +0700

    Xây dựng cấu trúc một block

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import * as CryptoJS from 'crypto-js';
+
+//cấu trúc 1 block
+class Block {
+
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public timestamp: number;
+    public data: string;
+
+    constructor(index: number, hash: string, previousHash: string, timestamp: number, data: string) {
+        this.index = index;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.hash = hash;
+    }
+}
+
