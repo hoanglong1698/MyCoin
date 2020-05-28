1c561f74064bba5793b54931494b006b4e0d8b2e
Hoang Long <1612352@student.hcmus.edu.vn>
2020-05-28 17:34:50 +0700

    Xây dựng cấu trúc lớp Đầu ra giao dịch chưa chi tiêu (UnspentTxOut)

--- src/transaction.ts
+++ src/transaction.ts
@@ -6,6 +6,7 @@ const ec = new ecdsa.ec('secp256k1');
 
 const COINBASE_AMOUNT: number = 50;
 
+//Đầu ra giao dịch chưa chi tiêu
 class UnspentTxOut {
     public readonly txOutId: string;
     public readonly txOutIndex: number;
@@ -55,4 +56,25 @@ const getTransactionId = (transaction: Transaction): string => {
         .reduce((a, b) => a + b, '');
 
     return CryptoJS.SHA256(txInContent + txOutContent).toString();
+};
+
+//Hàm ký đối tượng TxIn
+const signTxIn = (transaction: Transaction, txInIndex: number, privateKey: string, aUnspentTxOuts: UnspentTxOut[]): string => {
+    const txIn: TxIn = transaction.txIns[txInIndex];
+    const dataToSign = transaction.id;
+    const referencedUnspentTxOut: UnspentTxOut = findUnspentTxOut(txIn.txOutId, txIn.txOutIndex, aUnspentTxOuts);
+    const referencedAddress = referencedUnspentTxOut.address;
+    const key = ec.keyFromPrivate(privateKey, 'hex');
+    const signature: string = toHexString(key.sign(dataToSign).toDER());
+    return signature;
+};
+
+const findUnspentTxOut = (transactionId: string, index: number, aUnspentTxOuts: UnspentTxOut[]): UnspentTxOut => {
+    return aUnspentTxOuts.find((uTxO) => uTxO.txOutId === transactionId && uTxO.txOutIndex === index);
+};
+
+const toHexString = (byteArray): string => {
+    return Array.from(byteArray, (byte: any) => {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('');
 };
