b5b71c77a9dfff1030d2f79e9cfe5ca5044b3560
Hoang Long <1612352@student.hcmus.edu.vn>
2020-05-28 18:07:54 +0700

    Hàm xác thực cấu trúc một transaction và các biến có đúng kiểu dữ liệu

--- src/transaction.ts
+++ src/transaction.ts
@@ -97,3 +97,83 @@ const updateUnspentTxOuts = (newTransactions: Transaction[], aUnspentTxOuts: Uns
 
     return resultingUnspentTxOuts;
 };
+
+//Hàm xác thực 1 transaction có đúng cấu trúc và các biến có đúng kiểu dữ liệu hay không
+const isValidTransactionStructure = (transaction: Transaction) => {
+    if (typeof transaction.id !== 'string') {
+        console.log('transactionId missing');
+        return false;
+    }
+    if (!(transaction.txIns instanceof Array)) {
+        console.log('invalid txIns type in transaction');
+        return false;
+    }
+    if (!transaction.txIns
+            .map(isValidTxInStructure)
+            .reduce((a, b) => (a && b), true)) {
+        return false;
+    }
+
+    if (!(transaction.txOuts instanceof Array)) {
+        console.log('invalid txIns type in transaction');
+        return false;
+    }
+
+    if (!transaction.txOuts
+            .map(isValidTxOutStructure)
+            .reduce((a, b) => (a && b), true)) {
+        return false;
+    }
+    return true;
+}; 
+
+const isValidTxInStructure = (txIn: TxIn): boolean => {
+    if (txIn == null) {
+        console.log('txIn is null');
+        return false;
+    } else if (typeof txIn.signature !== 'string') {
+        console.log('invalid signature type in txIn');
+        return false;
+    } else if (typeof txIn.txOutId !== 'string') {
+        console.log('invalid txOutId type in txIn');
+        return false;
+    } else if (typeof  txIn.txOutIndex !== 'number') {
+        console.log('invalid txOutIndex type in txIn');
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const isValidTxOutStructure = (txOut: TxOut): boolean => {
+    if (txOut == null) {
+        console.log('txOut is null');
+        return false;
+    } else if (typeof txOut.address !== 'string') {
+        console.log('invalid address type in txOut');
+        return false;
+    } else if (!isValidAddress(txOut.address)) {
+        console.log('invalid TxOut address');
+        return false;
+    } else if (typeof txOut.amount !== 'number') {
+        console.log('invalid amount type in txOut');
+        return false;
+    } else {
+        return true;
+    }
+};
+
+//valid address is a valid ecdsa public key in the 04 + X-coordinate + Y-coordinate format
+const isValidAddress = (address: string): boolean => {
+    if (address.length !== 130) {
+        console.log('invalid public key length');
+        return false;
+    } else if (address.match('^[a-fA-F0-9]+$') === null) {
+        console.log('public key must contain only hex characters');
+        return false;
+    } else if (!address.startsWith('04')) {
+        console.log('public key must start with 04');
+        return false;
+    }
+    return true;
+};
